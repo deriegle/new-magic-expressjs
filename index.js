6aede64b3829a0a5141418a4cb14cd0d927d10e9
Devin Riegle <riegledevin@gmail.com>
2020-12-24 03:58:45 -0500

    Move messages routes to separate Router

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const turboStream = require('./turboStream');
 const formidable = require('express-formidable');
 const path = require('path');
 const Message = require('./message');
+const messagesRouter = require('./routes/messages');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Used to load styles.css file
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Used for collecting data from form submissions
 app.use(formidable());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const viewsPath = path.join(__dirname, './views');
+
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
 
@@ -29,76 +31,10 @@ app.use((_req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    const { messageId } = req.params;
+app.get('/', (_req, res) => res.render('index', {
+    messages: Message.all(),
+}));
 
-    if (messageId) {
-        const message = Message.findById(messageId);
-
-        if (!message) {
-            return res.writeHead(404);
-        }
-    }
-
-    next();
-});
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        messages: Message.all(),
-    })
-});
-
-// POST /messages
-// 
-// Creates a new message
-app.post('/messages', (req, res) => {
-    const { content } = req.fields || {};
-
-    const message = Message.create(content || '');
-
-    res.turboStream.append('messages', {
-        partial: 'messages/show',
-        locals: {
-            message,
-        }
-    })
-});
-
-// GET /messages/:messageId/edit
-//
-// Renders edit view for a particular message using turbo streams
-app.get('/messages/:messageId/edit', (req, res) => {
-    const message = Message.findById(req.params.messageId); 
-
-    res.render('messages/edit', {
-        message,
-    })
-});
-
-// POST /messages/:messageId
-//
-// Updates existing message content
-
-app.post('/messages/:messageId', (req, res) => {
-    const { content } = req.fields || {};
-    const message = Message.updateById(req.params.messageId, content);
-
-    res.render('messages/show', {
-        message,
-    });
-});
-
-// POST /messages/:messageId/delete
-//
-// Endpoint for deleting a message (since you can't do deletes from form submissions)
-// We should probably convert this over to using methodOverride instead
-app.post('/messages/:messageId/delete', (req, res) => {
-    const { messageId } = req.params;
-
-    Message.removeById(messageId);
-
-    res.turboStream.remove(`message_${messageId}`);
-});
+app.use('/messages', messagesRouter)
 
 app.listen(PORT, () => console.log('Listening on port 3001'));
---
+++ routes/messages.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const Message = require('../message');
+
+router.param('messageId', (_req, _res, next, messageId) => {
+    const message = Message.findById(messageId);
+
+    if (message) {
+        next();
+    } else {
+        next(new Error('Message not found.'));
+    }
+});
+
+// POST /messages
+// 
+// Creates a new message
+router.post('/', (req, res) => {
+    const { content } = req.fields || {};
+
+    const message = Message.create(content || '');
+
+    res.turboStream.append('messages', {
+        partial: 'messages/show',
+        locals: {
+            message,
+        }
+    })
+});
+
+// GET /messages/:messageId/edit
+//
+// Renders edit view for a particular message using turbo streams
+router.get('/:messageId/edit', (req, res) => {
+    const message = Message.findById(req.params.messageId); 
+
+    res.render('messages/edit', {
+        message,
+    })
+});
+
+// POST /messages/:messageId
+//
+// Updates existing message content
+router.post('/:messageId', (req, res) => {
+    const { content } = req.fields || {};
+    const message = Message.updateById(req.params.messageId, content);
+
+    res.render('messages/show', {
+        message,
+    });
+});
+
+// POST /messages/:messageId/delete
+//
+// Endpoint for deleting a message (since you can't do deletes from form submissions)
+// We should probably convert this over to using methodOverride instead
+router.post('/:messageId/delete', (req, res) => {
+    const { messageId } = req.params;
+
+    Message.removeById(messageId);
+
+    res.turboStream.remove(`message_${messageId}`);
+});
+
+module.exports = router;
